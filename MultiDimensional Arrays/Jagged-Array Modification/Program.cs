657710abb3bfa06ecd3b18e9ca9dc090d61b2683
unknown <embakks@gmail.com>
2022-05-23 00:09:33 +0300

    jagged array

---
+++ MultiDimensional Arrays/Jagged-Array Modification/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+
+namespace Jagged_Array_Modification
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[][] jagged = new int[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                jagged[i] = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            }
+
+            string input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string[] command = input.Split();
+
+                string commandType = command[0];
+                int row = int.Parse(command[1]);
+                int col = int.Parse(command[2]);
+                int value = int.Parse(command[3]);
+
+                if (row >= n || row < 0)
+                {
+                    Console.WriteLine("Invalid coordinates");
+                    continue;
+                }
+                if (col >= jagged[row].Length || col < 0)
+                {
+                    Console.WriteLine("Invalid coordinates");
+                    continue;
+                }
+
+                if (commandType == "Add")
+                {
+                    jagged[row][col] += value;
+                }
+                else if (commandType == "Subtract")
+                {
+                    jagged[row][col] -= value;
+                }
+                }
+
+            foreach (var arr in jagged)
+            {
+                Console.WriteLine(String.Join(" ", arr));
+            }
+        }
+    }
+}
---
+++ MultiDimensional Arrays/Pascal Triangle/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Pascal_Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[][] jagged = new int[n][];
+
+            jagged[0] = new int[1] {1};
+
+            for (int row = 1; row < n; row++)
+            {
+                jagged[row] = new int[jagged[row - 1].Length + 1];
+
+                for (int col = 0; col < jagged[row].Length; col++)
+                {
+                    if (col - 1 < 0 || col + 1 > jagged[row - 1].Length)
+                    {
+                        jagged[row][col] = 1;
+                    }
+                    else
+                    {
+                        jagged[row][col] = jagged[row - 1][col - 1] + jagged[row - 1][col];
+                    }
+                }
+            }
+
+            foreach (var arr in jagged)
+            {
+                Console.WriteLine(string.Join(" ", arr));
+            }
+        }
+    }
+}
