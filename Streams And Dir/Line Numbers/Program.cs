a67383344a080f4e554cc918f31bc8ed24d0ca04
unknown <embakks@gmail.com>
2022-05-31 00:01:33 +0300

    Line Numbers Exercise

---
+++ Streams And Dir/Line Numbers/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+
+namespace LineNumbers
+{
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\Files\input.txt";
+            string outputFilePath = @"..\..\..\Files\output.txt";
+
+            RewriteFileWithLineNumbers(inputFilePath, outputFilePath);
+        }
+
+        public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+        {
+            StreamReader reader = new StreamReader(inputFilePath);
+
+            using (reader)
+            {
+                StreamWriter writer = new StreamWriter(outputFilePath);
+
+                using (writer)
+                {
+                    int counter = 0;
+
+                    while (true)
+                    {
+                        string line = reader.ReadLine();
+
+                        if (line == null)
+                            break;
+
+                        writer.WriteLine(counter + ". " + line);
+                        counter++;
+                    }
+                }
+            }
+        }
+    }
+}
+
