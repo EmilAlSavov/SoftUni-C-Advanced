47ff2867fbb72ca8115964a7987578358d988a40
unknown <embakks@gmail.com>
2022-05-30 23:47:18 +0300

    Streams And Directories

---
+++ Streams And Dir/Streams And Dir/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+
+namespace OddLines
+{
+    public class OddLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\Files\input.txt";
+            string outputFilePath = @"..\..\..\Files\output.txt";
+
+            ExtractOddLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+            // TODO: write your code here…
+
+            StreamReader streamReader = new StreamReader(inputFilePath);
+
+            using (streamReader)
+            {
+                int lineCount = 0;
+
+                StreamWriter writer = new StreamWriter(outputFilePath);
+
+                using (writer)
+                {
+                    while (true)
+                    {
+                        string line = streamReader.ReadLine();
+                        if (line == null)
+                        {
+                            break;
+                        }
+                        if (lineCount % 2 != 0)
+                        {
+                            writer.WriteLine(line);
+                        }
+                        lineCount++;
+                    }
+                }
+            }
+        }
+    }
+}
+
---
+++ Streams And Directories/Streams And Directories/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Streams_And_Directories
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
