ac81253fb55384eefa7d64c2481a33bd23de6432
unknown <embakks@gmail.com>
2022-06-08 13:06:10 +0300

    Defining Classess

---
+++ Defining Classes/Define a Class Person/Family.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Define_a_Class_Person
+{
+    internal class Family
+    {
+        public Family()
+        {
+            fam = new List<Person>();
+        }
+        private List<Person> fam;
+
+        public void AddMember(Person p)
+        {
+            fam.Add(p);
+        }
+
+        public Person GetOldestMember()
+        {
+            int minVal = int.MinValue;
+            Person per = null;
+
+            foreach (var p in fam)
+            {
+                if (p.Age > minVal)
+                {
+                    minVal = p.Age;
+                    per = p;
+                }
+            }
+            return per;
+        }
+    }
+}
---
+++ Defining Classes/Define a Class Person/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Define_a_Class_Person
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Family savovi = new Family();
+            int n = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                string name = input[0];
+                int age = int.Parse(input[1]);
+
+                savovi.AddMember(new Person(name, age));
+            }
+
+            Console.WriteLine(savovi.GetOldestMember().Name + " " + savovi.GetOldestMember().Age);
+        }
+    }
+}
